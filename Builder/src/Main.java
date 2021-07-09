f70d370c0d1d9375e5cf7eacb933824b6512ca6e
Alexander Filyukov <filyukov@yandex.ru>
2021-07-09 18:48:38 +0300

    Done with Builder

--- Builder/src/Main.java
+++ Builder/src/Main.java
@@ -1,2 +1,26 @@
+import java.lang.System;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Client client1 = new Client.ClientBuilder().clientId(101).clientName("Alex.").build();
+            System.out.println(client1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            Client client2 = new Client.ClientBuilder().clientName("Alex.").build();
+            System.out.println(client2);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            Client client3 = new Client.ClientBuilder().clientId(383).clientName("Masha").departName("Regions").build();
+            System.out.println(client3);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
 }
